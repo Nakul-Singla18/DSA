1f66c71c48282526a8a65de60fec52c8e4c5aa54
Nakul Singla <132779373+Nakul-Singla18@users.noreply.github.com>
2024-06-13 00:52:58 +0530

    Create solution.java

---
+++ june/13 june/217. Contains Duplicate/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set= new HashSet<>();
+        for(int n: nums){
+            if(set.contains(n)){
+                return true;
+            }
+            set.add(n);
+        }
+        return false;
+    }
+}
